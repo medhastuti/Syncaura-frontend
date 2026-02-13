ad9623b3123666771ac62e7898cb1eed28f216e7
pruthweesh <pruthweesh2006@gmail.com>
2026-02-13 07:19:47 +0530

    reportRoute creation

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import themeReducer from "./slices/themeSlice";
 import meetingReducer from "./slices/meetingSlice";
 import notificationReducer from "./slices/notificationSlice";
 import documentReducer from "./slices/documentSlice";
+import reportReducer from "./slices/reportSlice"; // ← Did you add this?
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     meeting: meetingReducer,
     notification: notificationReducer,
     documents: documentReducer,
+    reports: reportReducer, // ← And this?
   },
 });
